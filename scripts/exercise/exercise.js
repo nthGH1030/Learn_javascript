504b5568a4a62095b84d66a9ca144fb7f9deb1ab
Kelvin Ngan <nth1030@gmail.com>
2024-02-11 16:57:19 +0800

    Exercise 15c + 15d

--- scripts/exercise/exercise.js
+++ scripts/exercise/exercise.js
@@ -10,8 +10,18 @@ export function exercise()
     console.log(`Exercise 15a: ${fiveDaysAfterDateString}`);
 
     //exercise 15b
-    const oneMonAfter = today.add(1, 'month');
-    const oneMonthAfterDateString = oneMonAfter.format('MMMM D');
+    const oneMonthAfter = today.add(1, 'month');
+    const oneMonthAfterDateString = oneMonthAfter.format('MMMM D');
     console.log(`Exercise 15b: ${oneMonthAfterDateString}`);
+
+
+    //exercise 15c
+    const oneMonthBefore = today.subtract(1, 'month');
+    const oneMonthBeforeDateString = oneMonthBefore.format('MMMM D');
+    console.log(`Exercise 15c: ${oneMonthBeforeDateString}`);
+
+    //exercise 15d
+    const dayOfWeekDateString = today.format('dddd');
+    console.log(dayOfWeekDateString);
 
 }
