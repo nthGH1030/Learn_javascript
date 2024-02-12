cd302203c53d7f2b1119a234b2eda2e46383817f
Kelvin Ngan <nth1030@gmail.com>
2024-02-12 11:07:17 +0800

    exercise 15i

--- scripts/checkout/paymentSummary.js
+++ scripts/checkout/paymentSummary.js
@@ -7,11 +7,14 @@ import {formatCurrency} from '../utils/money.js';
 export function renderPaymentSummary(){
     let productPriceCents = 0;
     let shippingPriceCents = 0;
+    let cartTotalQuantity = 0;
 
     cart.forEach((cartItem) =>{
         const product = getProduct(cartItem.productId);
         productPriceCents += 
         product.priceCents * cartItem.quantity;
+        cartTotalQuantity += cartItem.quantity;
+        
 
         const deliveryOption = getDeliveryOption(cartItem.deliveryOptionId);
         shippingPriceCents += deliveryOption.priceCents;
@@ -28,7 +31,7 @@ export function renderPaymentSummary(){
         </div>
 
         <div class="payment-summary-row">
-            <div>Items (3):</div>
+            <div>Items (${cartTotalQuantity}):</div>
             <div class="payment-summary-money">
             ${formatCurrency(productPriceCents)}
             </div>
@@ -66,7 +69,7 @@ export function renderPaymentSummary(){
             Place your order
   </button>
   `;
-  
+
   document.querySelector('.js-payment-summary')
   .innerHTML = paymentSummaryHTML;
 }
